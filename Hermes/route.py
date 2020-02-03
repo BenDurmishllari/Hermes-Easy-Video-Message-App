00230cdccff0bd93d26e9d04693319a76a11b43f
Ben Durmishllari <ben.durmishllari@gmail.com>
2020-02-03 16:50:01 +0000

    home page buttons ok with the positions, moving to send video part

--- Hermes/route.py
+++ Hermes/route.py
@@ -4,3 +4,8 @@ from flask import render_template
 @app.route('/')
 def loginPage():
     return render_template('homePage.html')
+
+
+@app.route('/sendVideo')
+def sendVideoPage():
+    return render_template('sendVideoPage.html')
