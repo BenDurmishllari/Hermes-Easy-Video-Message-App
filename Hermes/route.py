df5a19c0192aadd687b4bc88764386152e5831fe
Ben Durmishllari <ben.durmishllari@gmail.com>
2020-02-06 15:00:46 +0000

    getUserMedia() doesn't response, probably because of the HTTP need to set an ssl to migrate to https

--- Hermes/route.py
+++ Hermes/route.py
@@ -1,11 +1,12 @@
 from Hermes import app
-from flask import render_template
+from flask import render_template, request, Response
 
 @app.route('/')
 def loginPage():
     return render_template('homePage.html')
 
 
-@app.route('/sendVideo', methods = ['GET', 'POST'])
+@app.route('/sendVideo')
 def sendVideoPage():
+
     return render_template('sendVideoPage.html')
