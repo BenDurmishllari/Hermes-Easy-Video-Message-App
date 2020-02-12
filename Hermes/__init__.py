e3f3710318d65c5e4a6bb1dcc202af4de947bc03
Ben Durmishllari <ben.durmishllari@gmail.com>
2020-02-12 17:47:02 +0000

    record video with audio, couln't send it back to the server

--- Hermes/__init__.py
+++ Hermes/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import os
 
+UPLOAD_FOLDER = '../upload_files'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app)
+
 
 
 from Hermes import route
--- Hermes/route.py
+++ Hermes/route.py
@@ -1,12 +1,41 @@
-from Hermes import app
+from Hermes import app, UPLOAD_FOLDER
 from flask import render_template, request, Response
+from werkzeug.utils import secure_filename
+import os
 
-@app.route('/')
+
+
+# @app.route('/')
+# def loginPage():
+#     return render_template('homePage.html')
+
+
+# @app.route('/sendVideo')
+# def sendVideoPage():
+
+#     return render_template('sendVideoPage.html')
+
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
 def loginPage():
-    return render_template('homePage.html')
 
-
-@app.route('/sendVideo')
-def sendVideoPage():
-
+    
+    
     return render_template('sendVideoPage.html')
+
+
+
+
+@app.route('/audiovideo',methods=['GET', 'POST'])
+def audiovideo():
+	if request.method == 'POST':
+		file = request.files['audiovideo']
+		filename = "myaudiovideo.webm"
+		filename = secure_filename(filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	return "success"
+
+
+
