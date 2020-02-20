64d586a406701500f0fdb3059fad3b1421e23cc1
Ben Durmishllari <ben.durmishllari@gmail.com>
2020-02-20 23:18:24 +0000

    setting the right priority of the steps

--- Hermes/route.py
+++ Hermes/route.py
@@ -1,37 +1,38 @@
 from Hermes import app, UPLOAD_FOLDER
-from flask import render_template, request, Response, jsonify
+from flask import render_template, request, Response, redirect
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
 
 
-
-
-# @app.route('/')
-# def loginPage():
-#     return render_template('homePage.html')
-
-
-# @app.route('/sendVideo')
-# def sendVideoPage():
-
-#     return render_template('sendVideoPage.html')
-
-
-
 @app.route('/')
-def loginPage():
+def login():
 	
-    return render_template('sendVideoPage.html')
+    return render_template('loginPage.html')
+
+@app.route('/homePage')
+def home():
+	
+    return render_template('homePage.html')
+
+@app.route('/createVideo', methods=['GET','POST'])
+def recordVideo():
+	
+    return render_template('createVideoPage.html')
+
+@app.route('/watchVideo')
+def watchVideo():
+		
+    return render_template('watchVideoPage.html')
 
 
 
 
-@app.route('/audiovideo',methods=['POST'])
+@app.route('/recordVideo',methods=['POST'])
 def audiovideo():
 	
 	if request.method == 'POST':
-		file = request.files['audiovideo']
+		file = request.files['video']
 		filename = "myaudiovideo.mp4"
 		filename = secure_filename(filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
