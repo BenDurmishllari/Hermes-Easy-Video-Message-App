9ea0d6c9c92853b1f8d0aaee3eb623431fad3552
Ben Durmishllari <ben.durmishllari@gmail.com>
2020-02-21 17:10:32 +0000

    refresh the files, add it on gitignore video folder

--- Hermes/__init__.py
+++ Hermes/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
+import config
 
 
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '/upload_files'
+UPLOAD_FOLDER = './Hermes/static/uploadVideos'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
+
+
+
 
 
 
