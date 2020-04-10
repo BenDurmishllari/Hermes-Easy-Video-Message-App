c884e6df3480e79c6572f44cea22a763ba28a324
Ben Durmishllari <ben.durmishllari@gmail.com>
2020-04-10 02:24:12 +0100

    login validation works ok along with the boostrap danger,info,success messages also, the dic that handle the data for the inbox its reverse correct to render always the newest message on the top of the list

--- Hermes/__init__.py
+++ Hermes/__init__.py
@@ -18,7 +18,8 @@ login_manager = LoginManager(app)
 # login_manager.init_app(app)
 
 login_manager.login_view = 'login'
-# login_manager.login_message_category = 'info'
+
+login_manager.login_message_category = 'info'
 
 
 
@@ -36,31 +37,8 @@ config = {
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
-#UsercollectionReference = db.child("Users")
+
 storage = firebase.storage()
-
-# path_on_cloud = "videos"
-# path_on_local = "./Hermes/static/uploadVideos/myaudiovideo.mp4"
-# storage.child(path_on_cloud).put(path_on_local)
-# vide_url = storage.child(path_on_cloud).get_url()
-
-
-# email = input('email\n')
-# password = input('pass\n')
-
-# user = auth.create_user_with_email_and_password(email, password)
-
-# auth.get_account_info(user['idToken'])
-# print(auth.get_account_info(user['idToken']))
-
-# db = firebase.database()
-
-# data = {
-#     "name": "Mortimer 'Malaka' Malakas2"
-# }
-
-# results = db.child("users").push(data, user['idToken'])
-
 
 
 
--- Hermes/route.py
+++ Hermes/route.py
@@ -10,7 +10,9 @@ from flask import (render_template,
 				   Response, 
 				   redirect, 
 				   url_for, 
-				   session)
+				   session,
+				   flash,
+				   abort)
 from flask_login import (login_user, 
                          logout_user, 
                          login_required, 
@@ -75,48 +77,73 @@ def add_header(r):
 def login():
 
 	
-	message = ""
-	
-	
 	if current_user.is_authenticated:
 		return redirect(url_for('home'))
 
-	if request.method == "POST":
+	# if request.method == "POST":
 		
 
+	# 	email = request.form["LogInFormEmail"]
+	# 	password = request.form["LogInFormPassword"]
+		
+	# 	loginUser = auth.sign_in_with_email_and_password(email, password)
+		
+	# 	user_id = loginUser['localId']
+	# 	uEmailToken = loginUser['email']
+		
+	# 	getCurrentDB = db.child("Users").order_by_key().equal_to(user_id).limit_to_first(1).get()
+		
+	# 	if getCurrentDB != None:
+
+	# 		for gc in getCurrentDB.each():
+	# 			user = User(username = gc.val().get('username'),
+	# 					    email = gc.val().get('email'), 
+	# 						role = gc.val().get('role'), 
+	# 						id = gc.val().get('id'),
+	# 						profile_image = gc.val().get('profile_image'))
+		
+			
+	# 		login_user(user)
+	# 		print(current_user)
+			
+	# 		next_page = request.args.get('next')
+	# 		return redirect(next_page) if next_page else redirect(url_for('home'))
+	# 	else:
+	# 		flash('Login Unsuccessful. Please check username and password', 'danger')
+	# 	return redirect(url_for('login'))
+	# else:
+			
+	if request.method == 'POST':
+		
 		email = request.form["LogInFormEmail"]
 		password = request.form["LogInFormPassword"]
 		
-		loginUser = auth.sign_in_with_email_and_password(email, password)
-		
-		user_id = loginUser['localId']
-		uEmailToken = loginUser['email']
-		
-		getCurrentDB = db.child("Users").order_by_key().equal_to(user_id).limit_to_first(1).get()
-		
-		if getCurrentDB != None:
-
+		try:
+			
+			loginUser = auth.sign_in_with_email_and_password(email, password)
+			user_id = loginUser['localId']
+			uEmailToken = loginUser['email']
+			
+			getCurrentDB = db.child("Users").order_by_key().equal_to(user_id).limit_to_first(1).get()
+			
 			for gc in getCurrentDB.each():
 				user = User(username = gc.val().get('username'),
 						    email = gc.val().get('email'), 
 							role = gc.val().get('role'), 
 							id = gc.val().get('id'),
 							profile_image = gc.val().get('profile_image'))
-		
+				
+				login_user(user)
+				print(current_user)
+				
+				next_page = request.args.get('next')
 			
-			login_user(user)
-			print(current_user)
-			#print(session['_user_id'])
-			# print("-------------login")
-			# print(session)
-			next_page = request.args.get('next')
-			return redirect(next_page) if next_page else redirect(url_for('home'))
-		else:
-			message = "No User found!"
-		return redirect(url_for('login'))
-	else:
+				return redirect(next_page) if next_page else redirect(url_for('home'))
+		except:
 			
-		message = "Incorrect Password!"
+			flash('Login Unsuccessful. Please check email and password', 'danger')
+			return render_template('loginPage.html')
+	
 	return render_template('loginPage.html')
 
 #logout route
@@ -205,6 +232,9 @@ def audiovideo():
 @app.route('/createAccount', methods = ['GET', 'POST'])
 # @login_required
 def createAccount():
+
+	if current_user.get_role() != 'Admin':
+		abort(403)
 	
 	if request.method == 'POST':
 		
@@ -217,22 +247,47 @@ def createAccount():
 		profile_pic = pic_file.read()
 
 			
-		user = auth.create_user_with_email_and_password(email, password)
-		currentUserId = user['localId']
-		currentUserEmail = user['email']
+		# user = auth.create_user_with_email_and_password(email, password)
+		# currentUserId = user['localId']
+		# currentUserEmail = user['email']
 		
-		put_pic = storage.child("profile_pic/" + str(currentUserEmail)).child(str(currentUserId)).put(profile_pic)
-		#getCurrentPic = storage.child("profile_pic/" + str(currentUserEmail)).get_url(currentUserId)
-		getCurrentPic = storage.child("profile_pic/" + str(currentUserEmail) + "/" + str(currentUserId)).get_url(str(currentUserId))
-		data = {"username": username, 
-				"email": email, 
-				"role": role, 
-				"id": currentUserId, 
-				"profile_image": getCurrentPic}
+		# put_pic = storage.child("profile_pic/" + str(currentUserEmail)).child(str(currentUserId)).put(profile_pic)
+		# #getCurrentPic = storage.child("profile_pic/" + str(currentUserEmail)).get_url(currentUserId)
+		# getCurrentPic = storage.child("profile_pic/" + str(currentUserEmail) + "/" + str(currentUserId)).get_url(str(currentUserId))
+		# data = {"username": username, 
+		# 		"email": email, 
+		# 		"role": role, 
+		# 		"id": currentUserId, 
+		# 		"profile_image": getCurrentPic}
 		
-		createUserRef = db.child("Users").child(currentUserId).set(data)
-		return redirect(url_for('home'))
+		# createUserRef = db.child("Users").child(currentUserId).set(data)
+		# flash('Account has been created', 'success')
+		# return redirect(url_for('home'))
+
+		try:
+			
+			user = auth.create_user_with_email_and_password(email, password)
+			currentUserId = user['localId']
+			currentUserEmail = user['email']
 		
+			put_pic = storage.child("profile_pic/" + str(currentUserEmail)).child(str(currentUserId)).put(profile_pic)
+			#getCurrentPic = storage.child("profile_pic/" + str(currentUserEmail)).get_url(currentUserId)
+			getCurrentPic = storage.child("profile_pic/" + str(currentUserEmail) + "/" + str(currentUserId)).get_url(str(currentUserId))
+			data = {"username": username, 
+					"email": email, 
+					"role": role, 
+					"id": currentUserId, 
+					"profile_image": getCurrentPic}
+		
+			createUserRef = db.child("Users").child(currentUserId).set(data)
+			flash('Account has been created', 'success')
+			return redirect(url_for('home'))
+		
+		except:
+			
+			flash('Something was wrong, try again please', 'success')
+			return render_template('createAccountPage.html')
+
 	return render_template('createAccountPage.html')
 
 @app.route('/users', methods = ['GET', 'POST'])
@@ -293,19 +348,6 @@ def sendMessage(id):
 			# 	print("The files are the same")
 			# else:
 			# 	print("ERROR FILES NOT THE SAME")
-			
-			
-			
-			print("-----serverPrejson")
-			print(putVideo)
-
-			# data = {"sender_id": current_userId,
-			# 		"receiver_id": receiverId,
-			# 		"sender_name": current_userName,
-			# 		"date": str(date),
-			# 		"time": str(timeSt),
-			# 		"message_body": getVideoUrl,
-			# 		"profile_image": senderImageUrl}
 
 			data = {"sender_id": current_userId,
 					"receiver_id": receiverId,
@@ -321,6 +363,7 @@ def sendMessage(id):
 
 			os.remove(os.path.join(app.config['UPLOAD_FOLDER'], str(current_userId) + ".mp4"))
 
+			flash('Video Message has been send it successfully', 'success')
 			return redirect(url_for('home'))
 		
 		if 'btnBackChooseOtherUser' in request.form:
@@ -338,23 +381,26 @@ def inbox():
 	
 	current_userMessages = db.child("Messages").child(current_userId)
 
-	data = {}
-	for s in current_userMessages.get():
-		senderId = s.key()
-		sender = db.child("Messages").child(current_userId).child(senderId)
-		
-		for msgId in sender.get():
-			messageId = msgId.key()
+	try:
+
+		data = {}
+		for s in current_userMessages.get():
+			senderId = s.key()
+			sender = db.child("Messages").child(current_userId).child(senderId)
 			
-			messages = db.child("Messages").child(current_userId).child(senderId).child(messageId).get()
-			
-			data[messageId] = dict(messages.val())
-			
+			for msgId in sender.get():
+				messageId = msgId.key()
+				
+				messages = db.child("Messages").child(current_userId).child(senderId).child(messageId).get()
+				
+				data[messageId] = dict(messages.val())
 	
 		print(data)
 		
-	
-		
+	except:
+		noMsg = 'You don\'t have any video message yet.'
+		flash('No messages', 'danger')
+		return render_template('inboxPage.html', noMsg=noMsg)
 	return render_template('inboxPage.html', messages=data)
 
 
